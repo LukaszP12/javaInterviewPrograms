0cecfc9197418abf4b5e09d797880c83cce8e1bb
LukaszP <lukaszp4@onet.eu>
2019-08-09 02:15:08 +0200

    6th commit

--- src/DuplicateCharsInString.java
+++ src/DuplicateCharsInString.java
@@ -1,6 +1,7 @@
 //Write a program to find out duplicate or repeated characters in a
 //string, and calculate the count of repeatation.
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
@@ -9,19 +10,20 @@ public class DuplicateCharsInString {
 
     public void findDuplicateChars(String str){
 
-    Map<Character,Integer> dupMap = new HashMap<Character, Integer>();
-    char[] chrs = str.toCharArray();
-    for(Character ch:chrs){
-        if(dupMap.containsKey(ch)){
-            dupMap.put(ch,dupMap.get(ch)+1);
-        } else {
-            dupMap.put(ch,1);
+        Map<Character,Integer> dupMap = new HashMap<Character, Integer>();
+        char[] chrs = str.toCharArray();
+        for(Character c : chrs){
+            if(dupMap.containsKey(c)){
+                dupMap.put(c,dupMap.get(c)+1);
+            }else {
+                dupMap.put(c,1);
+            }
         }
-    }
+
         Set<Character> keys = dupMap.keySet();
-        for(Character ch:keys){
-            if(dupMap.get(ch) > 1){
-                System.out.println(ch+"---->"+dupMap.get(ch));
+        for (Character character : keys){
+            if(dupMap.get(character) > 1 ){
+                System.out.println( character + " repeats itself");
             }
         }
     }
