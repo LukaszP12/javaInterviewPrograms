5a22c25b46a20d3e72bcb824fb036f5dd0e7835c
LukaszP <lukaszp4@onet.eu>
2019-10-18 15:49:03 +0200

    32th commit

---
+++ src/GfG_smallestKNumbersInOrder.java
@@ -0,0 +1,49 @@
+// smallest elements in the same order using O(1) extra space
+
+public class GfG_smallestKNumbersInOrder {
+
+    // Function to print smallest k numbers in arr[0..n-1]
+    public static void printSmall(int arr[],int n, int k)
+    {
+        // For each arr[i] find whether
+        // it is a part of the n-smallest
+        // with insertion sort concept
+        for (int i=k; i<n;++i){
+            // find largest from top n-element
+            int max_var = arr[k-1];
+            int pos = k - 1;
+            for (int j = k - 2; j>= 0; j--){
+                if(arr[j] > max_var){
+                    max_var = arr[j];
+                    pos = j;
+                }
+            }
+
+            // If largest is greater than arr[i]
+            // shift all element one place left
+            if (max_var > arr[i]){
+                int j = pos;
+                while (j < k - 1) {
+                    arr[j] = arr[j+1];
+                    j++;
+                }
+                // make arr[k-1] = arr[i]
+                arr[k-1] = arr[i];
+            }
+        }
+
+        // print result
+        for (int i = 0; i < k; i++){
+            System.out.println(arr[i] + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1, 5, 8, 9, 6, 7, 3, 4, 2, 0};
+        int n = 10;
+        int k = 5;
+        printSmall(arr,n,k);
+    }
+
+}
