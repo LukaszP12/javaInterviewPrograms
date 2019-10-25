6f4fddb2ce7035aa258cd412c03772ad596f76b5
LukaszP <lukaszp4@onet.eu>
2019-10-25 14:48:06 +0200

    39th commit + countDiv

--- src/Codility/countDiv.java
+++ src/Codility/countDiv.java
@@ -8,8 +8,19 @@ public class countDiv {
 
     }
 
+    // recurrence
+    public static int divisorsInRange(int min, int max, int div){
+        if(min>max){
+            return 0;
+        }else
+        return (min%div == 0 ? 1 : 0) + divisorsInRange(min+1,max,div );
+    }
+
     public static void main(String[] args) {
+        System.out.println("The first solution");
         System.out.println(solution(6,11,2));
+        System.out.println("Now the second solution with recurrence");
+        System.out.println(divisorsInRange(6,11,2));
     }
 
 }
