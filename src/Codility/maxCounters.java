8a5b8f6f2e0eceaca101eea40cdc3b56c6c63fc8
LukaszP <lukaszp4@onet.eu>
2019-10-25 11:34:27 +0200

    37th commit + maxCounters

--- src/Codility/maxCounters.java
+++ src/Codility/maxCounters.java
@@ -1,31 +1,44 @@
 package Codility;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class maxCounters {
 
-    public int[] solution(int n, int[] A){
+    public static int[] solution(int n, int[] A){
 
         int[] result = new int[n];
 
+        int level = 0;
         int max = 0;
         boolean previousMax = false;
 
+        int aboveMax = 0;
+
         for (int i : A){
 
-            if (i == n+1){
-                if(!previousMax){
-                    //max else
-                    for (int j=0;j<result.length;j++) {
-                        result[j] = max;
-                    }
+            if (i == n + 1){
+                if(!previousMax) {
+                    level = level + max;
+                    max = 0;
+                    result = new int[n];
                     previousMax = true;
                 }
-            }else{
-                result[i-1]++;
-                max = Math.max(max, result[i-1]);
+            } else {
+                result[i - 1]++;
+                max = Math.max(max,result[i-1]);
                 previousMax = false;
             }
         }
 
         return result;
     }
+
+    public static void main(String[] args) {
+        int[] testData = {3,4,4,6,1,4,4};
+
+        System.out.println(Arrays.toString(solution(5,testData)));
+        
+    }
+
 }
